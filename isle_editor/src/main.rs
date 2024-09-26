6416c2f2bc39cdec62c1a5159bfc012520669497
Jordan LaPrise <officialtoxicdub@gmail.com>
2024-09-25 21:08:57 -0800

    test some things

--- isle_editor/src/main.rs
+++ isle_editor/src/main.rs
@@ -4,7 +4,7 @@ use isle_engine::{entity::Entity, Scheduler};
 struct MyResource(pub usize);
 
 #[derive(Component, Debug)]
-struct MyComponentOne;
+struct MyComponentOne(usize);
 
 #[derive(Component, Debug)]
 struct MyComponentTwo;
@@ -20,29 +20,32 @@ fn main() {
 
     ecs.add_resource(MyResource(42));
 
-    ecs.add_component(Entity(0, 0), MyComponentOne);
+    ecs.add_component(Entity(0, 0), MyComponentOne(69));
     ecs.add_component(Entity(0, 0), MyComponentTwo);
     ecs.add_component(Entity(0, 0), MyComponentThree);
     ecs.add_component(Entity(0, 0), MyComponentFour);
 
-    ecs.add_component(Entity(0, 1), MyComponentOne);
+    ecs.add_component(Entity(0, 1), MyComponentOne(420));
     ecs.add_component(Entity(0, 1), MyComponentThree);
     ecs.add_component(Entity(0, 1), MyComponentFour);
 
+    ecs.add_system(my_complete_system);
     ecs.add_system(my_resource_system);
     ecs.add_system(my_query_system);
-    ecs.add_system(my_complete_system);
+    ecs.add_system(my_other_query_system);
 
     ecs.spin();
 }
 
 fn my_complete_system(
-    res: ResMut<MyResource>,
-    query: Query<(Entity, &MyComponentOne, &MyComponentFour), Without<MyComponentTwo>>,
+    mut res: ResMut<MyResource>,
+    query: Query<(Entity, &mut MyComponentOne, &MyComponentFour), Without<MyComponentTwo>>,
 ) {
     println!("Res is {}", res.0);
+    res.0 += 1;
     for (entity, one, four) in query.iter() {
         println!("<complete_system> Entity: {entity:?}, One: {one:?}, Four: {four:?}");
+        one.0 += 32;
     }
 }
 
@@ -60,5 +63,13 @@ fn my_query_system(
 ) {
     for (entity, one, four, two) in _query.iter() {
         println!("<query_system> Entity: {entity:?}, One: {one:?}, Two: {two:?}, Four: {four:?}");
+    }
+}
+
+fn my_other_query_system(
+    query: Query<Entity, With<MyComponentTwo>>
+) {
+    for entity in query.iter() {
+        println!("<other_query_system> Entity: {entity:?}");
     }
 }
