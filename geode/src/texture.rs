fbb6bc4fd8e9a8e36d8823337a728e4fab8a9ca0
Jordan <jlaprise@blakwurm.com>
2024-10-17 16:17:49 -0800

    Implement incremental texture loading

--- geode/src/texture.rs
+++ geode/src/texture.rs
@@ -34,6 +34,105 @@ pub struct Texture {
 impl Texture {
     pub const DEPTH_FORMAT: wgpu::TextureFormat = wgpu::TextureFormat::Depth32Float;
 
+    pub fn new(path: &Path) -> Self {
+        Texture {
+            source: TextureSource::Disk(path.to_path_buf()),
+            state: TextureState::Disk,
+            size: Vec2(0.0, 0.0),
+        }
+    }
+
+    pub fn name(&self) -> &str {
+        match &self.source {
+            TextureSource::Disk(path) => path.file_name().unwrap().to_str().unwrap(),
+            TextureSource::Internal(name) => name,
+            TextureSource::Dynamic(name) => name,
+        }
+    }
+
+    pub fn load_to_mem(&mut self) -> Result<(), ImageError> {
+        let path = if let TextureSource::Disk(path) = &self.source {
+            path
+        } else {
+            log::warn!("Attempted to load non-disk '{}' texture to memory", self.name());
+            return Ok(());
+        };
+
+        let image = image::open(path)?;
+        self.size = image.dimensions().into();
+        self.state = TextureState::Memory(image);
+
+        Ok(())
+    }
+
+    pub fn load_to_gpu(&mut self, device: &wgpu::Device, queue: &wgpu::Queue) {
+        let image = match &self.state {
+            TextureState::Memory(image) => image,
+            TextureState::Gpu(_) => {
+                log::warn!("Attempted to load already loaded '{}' texture to GPU", self.name());
+                return;
+            }
+            TextureState::Atlased(_) => {
+                log::warn!("Attempted to load atlased '{}' texture to GPU", self.name());
+                return;
+            }
+            TextureState::Disk => {
+                log::error!("Attempted to load unloaded '{}' texture to GPU", self.name());
+                panic!("Attempted to load unloaded texture to GPU");
+            }
+        };
+
+        let rgba = image.to_rgba8();
+        let size = wgpu::Extent3d {
+            width: self.size.0 as u32,
+            height: self.size.1 as u32,
+            depth_or_array_layers: 1,
+        };
+        let texture = device.create_texture(
+            &wgpu::TextureDescriptor {
+                label: Some(self.name()),
+                size,
+                mip_level_count: 1,
+                sample_count: 1,
+                dimension: wgpu::TextureDimension::D2,
+                format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+                view_formats: &[],
+            }
+        );
+
+        queue.write_texture(
+            wgpu::ImageCopyTexture {
+                aspect: wgpu::TextureAspect::All,
+                texture: &texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+            },
+            &rgba,
+            wgpu::ImageDataLayout {
+                offset: 0,
+                bytes_per_row: Some(4 * size.width),
+                rows_per_image: Some(size.height),
+            },
+            size,
+        );
+
+        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let sampler = device.create_sampler(
+            &wgpu::SamplerDescriptor {
+                address_mode_u: wgpu::AddressMode::ClampToEdge,
+                address_mode_v: wgpu::AddressMode::ClampToEdge,
+                address_mode_w: wgpu::AddressMode::ClampToEdge,
+                mag_filter: wgpu::FilterMode::Linear,
+                min_filter: wgpu::FilterMode::Nearest,
+                mipmap_filter: wgpu::FilterMode::Nearest,
+                ..Default::default()
+            }
+        );
+
+        self.state = TextureState::Gpu(GpuTexture { texture, view, sampler});
+    }
+
     pub fn create_depth_texture(device: &wgpu::Device, config: &wgpu::SurfaceConfiguration) -> Self {
         let size = wgpu::Extent3d {
             width: config.width.max(1),
--- isle_math/src/vector/d2.rs
+++ isle_math/src/vector/d2.rs
@@ -12,3 +12,15 @@ impl Into<(f32, f32)> for Vec2 {
         (self.0, self.1)
     }
 }
+
+impl From<(u32, u32)> for Vec2 {
+    fn from((x, y): (u32, u32)) -> Self {
+        Self(x as f32, y as f32)
+    }
+}
+
+impl Into<(u32, u32)> for Vec2 {
+    fn into(self) -> (u32, u32) {
+        (self.0 as u32, self.1 as u32)
+    }
+}
