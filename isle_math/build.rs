614117bc23b0ccccbf091efb7a8b06209b9319ed
Jordan <jlaprise@blakwurm.com>
2024-10-25 08:09:02 -0800

    cargo fmt

--- isle_math/build.rs
+++ isle_math/build.rs
@@ -4,8 +4,7 @@ fn main() {
     let data = generate_swizzle_macro();
     let out_dir = env::var("OUT_DIR").unwrap();
     let out_path = Path::new(&out_dir).join("swizzle_macro.rs");
-    let mut file = File::create(out_path)
-        .expect("Could not create swizzle_macro.rs");
+    let mut file = File::create(out_path).expect("Could not create swizzle_macro.rs");
     file.write_all(data.as_bytes())
         .expect("Could not write to swizzle_macro.rs");
 }
@@ -15,20 +14,22 @@ fn generate_swizzle_macro() -> String {
     for i in 2..5 {
         match_arms += &generate_swizzle_macro_arm(i);
     }
-    
-    format!("macro_rules! impl_swizzle {{
+
+    format!(
+        "macro_rules! impl_swizzle {{
         {match_arms}
-    }}")
+    }}"
+    )
 }
 
 fn generate_fn_params(pattern: &Vec<char>) -> String {
     let mut out = "".to_string();
-    
+
     for i in 0..pattern.len() {
         let char = pattern[i];
         out += &format!("self.{},", char);
     }
-    
+
     out
 }
 
@@ -36,32 +37,37 @@ fn generate_fn(components: &str, pattern: Vec<char>) -> String {
     let fn_params = generate_fn_params(&pattern);
     let fn_name = map_source(&components, &pattern);
     let len = pattern.len();
-    
-    format!("\
+
+    format!(
+        "\
     pub fn {fn_name}(&self) -> super::d{len}::Vec{len} {{
         super::d{len}::Vec{len}({fn_params})
         }}
-    \n").to_string()
+    \n"
+    )
+    .to_string()
 }
 
 fn generate_swizzle_macro_arm(components: u32) -> String {
     let mut vec = Vec::new();
     let s = "xyzw".to_string();
     let component_vec = &s[0..components as usize];
-    
-    for i in 2..components+1 {
+
+    for i in 2..components + 1 {
         vec.extend(generate_swizzle_n(component_vec, i as u32));
     }
-    
+
     let fns = vec.join("\n");
-    
-    format!("\
+
+    format!(
+        "\
         ($vector_type:ident, {components}) => {{
             impl $vector_type {{
                 {fns}
             }}
         }};
-    \n")
+    \n"
+    )
 }
 
 fn map_source(components: &str, pattern: &Vec<char>) -> String {
@@ -76,7 +82,7 @@ fn map_source(components: &str, pattern: &Vec<char>) -> String {
 fn generate_swizzle_n(components: &str, out_len: u32) -> Vec<String> {
     let b = components.len();
     let n = b.pow(out_len);
-    
+
     (0..n)
         .map(|i| to_base(i as u32, b as u32, out_len as usize))
         .map(|s| generate_fn(&components, s.chars().collect()))
@@ -85,12 +91,17 @@ fn generate_swizzle_n(components: &str, out_len: u32) -> Vec<String> {
 
 fn to_base(mut number: u32, base: u32, pad: usize) -> String {
     let mut result = String::new();
-    
+
     while number > 0 {
         let remainder = number % base;
         result.push_str(&remainder.to_string());
         number /= base;
     }
-    
-    format!("{:0>pad$}", result.chars().rev().collect::<String>(), pad=pad).into()
+
+    format!(
+        "{:0>pad$}",
+        result.chars().rev().collect::<String>(),
+        pad = pad
+    )
+    .into()
 }
--- isle_math/src/lib.rs
+++ isle_math/src/lib.rs
@@ -1,4 +1,4 @@
+mod macros;
 pub mod matrix;
 pub mod rotation;
 pub mod vector;
-mod macros;
